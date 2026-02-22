d669c5d85b1c990ea93536d75af9b335c19c2df7
NorthSideBoy <saidlucena519@gmail.com>
2026-02-22 02:44:47 -0400

    chore(docker): bump MongoDB to 8.0 and add healthcheck/volumes

--- docker/rs-init.sh
+++ docker/rs-init.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Wait for MongoDB to be ready
-until mongosh --host mongodb --port "$DB_PORT" -u "$DB_USER" -p "$DB_PASSWORD" --eval "db.adminCommand('ping')" >/dev/null 2>&1; do
-  echo "Waiting for MongoDB to be ready..."
-  sleep 2
-done
-
 mongosh --host mongodb --port "$DB_PORT" -u "$DB_USER" -p "$DB_PASSWORD" <<EOF2
 rs.initiate({
   _id: 'rs0',
